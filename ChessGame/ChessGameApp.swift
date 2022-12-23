b49099fbce03fe9d82ffd164e309ab4dd8e5d068
Caleb Marquart <marquartcaleb@gmail.com>
2022-12-23 09:28:48 -0700

    Initial Commit

---
+++ ChessGame/ChessGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChessGameApp.swift
+//  ChessGame
+//
+//  Created by Caleb Marquart on 2022-12-23.
+//
+
+import SwiftUI
+
+@main
+struct ChessGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
